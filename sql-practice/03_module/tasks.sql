a106617cbfd62b1b346ee76def4082f7ec92b807
tambekov <bekov.tembulat.y@yandex.ru>
2026-02-12 18:29:04 +0300

    SQL: finish the 03_module

--- sql-practice/03_module/tasks.sql
+++ sql-practice/03_module/tasks.sql
@@ -60,3 +60,65 @@ INNER JOIN
         ON c.UserID = d.UserID
 GROUP BY
     i.Source;
+
+-- 7
+SELECT
+    *
+FROM
+    devices AS d
+JOIN
+    checks AS c
+        ON d.UserID = c.UserID
+WHERE
+    toStartOfMonth(CAST(c.BuyDate AS Date)) = Date '2024-03-01'
+LIMIT 1
+
+-- 8
+SELECT
+    i.Source AS source_,
+    i.Platform AS platform_,
+    AVG(events) AS avg_events
+FROM
+    installs AS i
+INNER JOIN
+    events AS e
+        ON i.DeviceID = e.DeviceID
+GROUP BY
+    i.Source,
+    i.Platform
+ORDER BY
+    avg_events DESC;
+
+-- 9
+SELECT
+    i.Platform AS platform_,
+    uniqExact(i.DeviceID) as unique_device_ids
+FROM
+    installs AS i
+LEFT SEMI JOIN
+    events AS e
+        ON i.DeviceID = e.DeviceID
+GROUP BY
+    i.Platform
+
+-- 10
+SELECT
+   uniqExact(e.DeviceID) / uniqExact(i.DeviceID)
+FROM
+    installs AS i
+LEFT JOIN
+    events AS e
+        ON i.DeviceID = e.DeviceID
+
+
+-- 11
+SELECT
+    e.DeviceID
+FROM
+    events AS e
+LEFT ANTI JOIN
+    devices AS d
+        ON d.DeviceID = e.DeviceID
+ORDER BY
+    e.DeviceID DESC
+LIMIT 10;
