191d419a3db302b511ed5a88a4ae72ef5b3bc126
tambekov <bekov.tembulat.y@yandex.ru>
2026-02-17 17:45:45 +0300

    SQL: finish the 05_module

--- sql-practice/05_module/tasks.sql
+++ sql-practice/05_module/tasks.sql
@@ -58,3 +58,118 @@ WHERE
 	toDateOrNull(l.last_review) > DATE '2023-01-12'
 ORDER BY
 	price_ DESC
+
+-- 4
+WITH (
+SELECT
+	AVG(reviewers)
+FROM (
+	SELECT
+		r.listing_id,
+		uniqExact(r.reviewer_id) AS reviewers
+	FROM
+		`default`.reviews AS r
+	INNER JOIN
+		`default`.calendar_summary cs
+			ON cs.listing_id  = r.listing_id 
+	WHERE
+		cs.available = 't'
+	GROUP BY
+		r.listing_id
+)) AS avg_reviewers
+SELECT
+	uniqExact(r.reviewer_id) AS reviewers
+FROM
+	`default`.reviews AS r
+INNER JOIN
+	`default`.calendar_summary cs
+		ON cs.listing_id = r.listing_id
+WHERE
+	cs.available = 't'
+GROUP BY
+	r.listing_id
+HAVING
+	reviewers > avg_reviewers
+
+-- 5
+SELECT
+	CASE
+		WHEN avg_rub < 5 THEN 'A'
+		WHEN avg_rub < 10 THEN 'B'
+		WHEN avg_rub < 20 THEN 'C'
+		ELSE 'D'
+	END AS segment_name,
+	UserID
+FROM
+(
+	SELECT
+		c.UserID,
+		AVG(c.Rub) AS avg_rub
+	FROM
+		`default`.checks AS c
+	GROUP BY
+		c.UserID
+)
+ORDER BY UserID
+
+-- 6
+SELECT
+	segment_name,
+	SUM(total) AS total
+FROM
+(
+	SELECT
+		CASE
+			WHEN avg_rub < 5 THEN 'A'
+			WHEN avg_rub < 10 THEN 'B'
+			WHEN avg_rub < 20 THEN 'C'
+			ELSE 'D'
+		END AS segment_name,
+		UserID,
+		total
+	FROM
+	(
+		SELECT
+			c.UserID,
+			AVG(c.Rub) AS avg_rub,
+			SUM(c.Rub) AS total
+		FROM
+			`default`.checks AS c
+		GROUP BY
+			c.UserID	
+	)
+)
+GROUP BY
+	segment_name
+ORDER BY
+	total DESC
+
+-- PART 2
+
+-- 1
+CREATE TABLE test.reviews(
+	listing_id UInt32,
+	id UInt32,
+	date Datetime('Europe/Moscow'),
+	reviewer_id UInt32,
+	reviewer_name String,
+	comments String
+) ENGINE = MergeTree
+ORDER BY (listing_id, id)
+
+-- 2
+ALTER TABLE test.reviews
+MODIFY COLUMN date Date
+
+-- 3
+ALTER TABLE test_reviews
+DELETE WHERE comments = '';
+
+-- 4
+ALTER TABLE test.reviews
+ADD COLUMN price Float32 AFTER comments
+
+-- 5
+ALTER TABLE test.reviews
+UPDATE price = price * 2
+WHERE date > '2019-01-01'
